4de5e7df2f8f61fce69aa0177cc81f70a1b7d6ab
梶原大進 <kajiwarataishin@kajiwarataishinnomacbook-pro.local>
2018-05-11 13:34:26 +0900

    Initial Commit

---
+++ refrigerator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  refrigerator
+//
+//  Created by 梶原大進 on 2018/05/11.
+//  Copyright © 2018年 梶原大進. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
